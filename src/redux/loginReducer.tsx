58d6d5b0e5159330c65d231b07aeb1f25cf76fab
maqweb <balikhinv@bk.ru>
2020-02-23 00:47:24 +0300

    added login

--- src/redux/loginReducer.tsx
+++ src/redux/loginReducer.tsx
@@ -1,16 +1,49 @@
+import {Dispatch} from "redux";
+import {api} from "../api/api";
+import {isAuthAC} from "./profileReducer";
+
+const SET_DATA = 'SET_DATA';
+
 interface IInitialState {
     title: string
+    email: string
+    password: string
+    rememberMe: boolean
 }
 
 const initialState: IInitialState = {
-    title: 'Login'
+    title: 'Login',
+    email: '', /*'nikolas@gmail.com'*/
+    password: '', /*'1234567890'*/
+    rememberMe: false,
 };
 
 const loginReducer = (state: IInitialState = initialState, action: any): IInitialState => {
     switch (action.type) {
+
+        case SET_DATA:
+            return {
+                ...state,
+                email: action.data.email,
+                password: action.data.password,
+                rememberMe: action.data.rememberMe
+            };
+
         default:
             return state;
     }
+};
+
+export const setDataAC = (data: any) => ({type: SET_DATA, data});
+
+export const sendDataTC: Function = (email: string, password: string, rememberMe: boolean) => (dispatch: Dispatch) => {
+    api.sendData(email, password, rememberMe)
+        .then(res => {
+            if (res.status === 200) {
+                dispatch(setDataAC(res.data));
+                dispatch(isAuthAC(true, res.data.name))
+            }
+        })
 };
 
 export default loginReducer
